f54065960b35d344ad673d192f5c71c575736568
asebova <seana-qa@ya.ru>
2024-07-13 21:01:27 +0300

    Добавляем в GitHub Action Аллюр отчет

--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -1,5 +1,8 @@
 package example;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -8,7 +11,9 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class ExampleTest {
     @Test
+    @Step
     void name() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open("https://docs.github.com/en/actions");
         $("#title-h1").shouldHave(text("Проверка"));
     }
