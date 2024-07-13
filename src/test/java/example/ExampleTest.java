e3f291a7bb13934034b0161e57af0c6d85770865
asebova <seana-qa@ya.ru>
2024-07-13 19:26:54 +0300

    Подключены тестовый .yml

--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -2,6 +2,7 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -9,6 +10,6 @@ public class ExampleTest {
     @Test
     void name() {
         open("https://docs.github.com/en/actions");
-        $("div.nastya").click();
+        $("#title-h1").shouldHave(text("GitHub Actions documentation"));
     }
 }
